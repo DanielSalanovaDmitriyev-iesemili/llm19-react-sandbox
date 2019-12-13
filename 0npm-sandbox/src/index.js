d4474d02330a7ab2e4b8cf50eca16df5f2921aef
Darkstar8217 <dsd00@iesemilidarder.com>
2019-12-13 16:16:39 +0100

    2

---
+++ 0npm-sandbox/src/index.js
@@ -0,0 +1,48 @@
+//deprecated...smells bad
+
+var name = "student";
+console.log("hola" + name);
+//Nowadars...let, const
+let realName = "Pepito palotes";
+const hiThere = "Aloha"; //non var
+console.log(hiThere + " " + realName);
+/*
+Multi line comment
+hiThere= "Xao;
+console.Log("Llego")
+ */
+/*for (let i=0; i< 1; i++) //for
+{
+    alert("molestooo"); //old html function
+}
+if(confirm("¿molesto?")){ //if + old confirm!!!
+    alert("lo se")      //annoying
+
+}*/
+/*
+Webstorm shortcodes
+ */
+//Var types
+let number = 1;
+let string = "1";
+let bool = true;
+let o = {"id":123, "name":"John"};
+console.log("Person: "+o.id+ " - "+o.name);
+//Brain wash area
+/*
+Function way 1
+ */
+function sayHI(){
+    console.log("Normal function!!!!");
+}
+sayHi();
+/*
+Function way 2
+ */
+let sayHiTwo = function(){
+    console.log("Function as a var!!!")
+};
+sayHiTwo();
+/*
+    +Function real men wat
+ */
